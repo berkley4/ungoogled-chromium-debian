26ee21e70df99992fca2b80970c36eae645a989d
berkley4 <jeffblake0@gmail.com>
2024-01-04 09:50:17 +0000

    Remove line break from devutils/update_launcher.sh

--- debian/devutils/update_launcher.sh
+++ debian/devutils/update_launcher.sh
@@ -2,6 +2,5 @@
 
 . debian/devutils/print_dist.inc
 
-sed \
-    -e "s|@BUILD_DIST@|$(print_dist)|" \
+sed -e "s|@BUILD_DIST@|$(print_dist)|" \
     -e "/@PRINT_DIST@/c\\$(sed '$!s|$|\\|' debian/devutils/print_dist.inc)"
