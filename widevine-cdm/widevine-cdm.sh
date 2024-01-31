edd2a43f4fbbf6b4a11e8ca7803dff71768f698a
berkley4 <jeffblake0@gmail.com>
2024-01-31 09:29:36 +0000

    Improve widevine-cdm/widevine-cdm.sh
    This commit replaces if..then clauses with case
    statements, uses a shell parameter expansion instead
    of an external dirname command, and gets rid of the
    d_ver intermediate variable.
    
    There is also some general tidying up of the script.

--- widevine-cdm/widevine-cdm.sh
+++ widevine-cdm/widevine-cdm.sh
@@ -3,59 +3,70 @@ set -e
 
 base_url=https://dl.google.com/linux/deb/pool/main/g/google-chrome-stable
 
-W_DIR=$(dirname $0)
-
-[ -n "$CHECK" ] || CHECK=0
-[ -n "$DL_CACHE" ] || DL_CACHE=$W_DIR/../download_cache
-
-if [ -z "$CHROME_VER" ]; then
-  printf '%s\n' "You must specify the CHROME_VER variable"
-  exit 1
-fi
+W_DIR=${0%/*}    # ${example%/*} = $(dirname example)
 
 
-## PREPARATION
+case $CHROME_VER in
+  "")
+    printf '%s\n' "Usage: CHROME_VER=<version> widevine-cdm.sh"
+    exit 1 ;;
+esac
+
+case $CHECK in
+  "")
+    CHECK=0 ;;
+esac
+
+case $DL_CACHE in
+  "")
+    DL_CACHE=$W_DIR/../download_cache ;;
+esac
+
+
+## MAKE NEEDED DIRECTORIES
 
 for dir in $DL_CACHE $W_DIR/DEB/usr/lib; do
   [ -d $dir ] || mkdir -p $dir
 done
 
 
-## DOWNLOADING
+## DOWNLOAD
 
 command -v aria2c >/dev/null 2>&1 && D_LOADER=aria2c || D_LOADER=wget
 
 dl_args="--continue -P $DL_CACHE"
-[ "$D_LOADER" = "wget" ] || dl_args="-x2 -s2 -c -d $DL_CACHE"
+
+case $D_LOADER in
+  aria2c)
+    dl_args="-x2 -s2 -c -d $DL_CACHE" ;;
+esac
 
 $D_LOADER $dl_args $base_url/google-chrome-stable_${CHROME_VER}_amd64.deb
 
 
-## FILE EXTRACTION
+## EXTRACT FILES
 
 printf '\n%s\n' "Extracting files....."
 
 ar p $DL_CACHE/google-chrome-stable_${CHROME_VER}_amd64.deb data.tar.xz | \
-  tar -C $W_DIR/DEB/usr/lib -xpJf - \
-      --strip-components 4 ./opt/google/chrome/WidevineCdm
+  tar -C $W_DIR/DEB/usr/lib -xpJf - --strip-components 4 ./opt/google/chrome/WidevineCdm
 
 
-## VERSION CHECKING
+## GET/CHECK WIDEVINE VERSION
 
 w_man=$W_DIR/DEB/usr/lib/WidevineCdm/manifest.json
 w_ver=$(sed -n 's@.*"version": "\([^"]*\).*@\1@p' $w_man)
 
 if [ $CHECK -eq 1 ]; then
-  d_ver=$(apt-cache show widevine-cdm 2>/dev/null | sed -n 's@^Version: @@p')
-
-  if [ "$w_ver" = "$d_ver" ]; then
-    printf '%s\n' "Installed version $d_ver matches upstream, not building."
-    exit 0
-  fi
+  case $w_ver in
+    $(apt-cache show widevine-cdm 2>/dev/null | sed -n 's@^Version: @@p'))
+      printf '%s\n' "Installed version $w_ver matches upstream, not building."
+      exit 0 ;;
+  esac
 fi
 
 
-## BUILDING
+## BUILD DEB PACKAGE
 
 sed "s/@@W_VERSION@@/$w_ver/" < $W_DIR/control.in > $W_DIR/DEB/DEBIAN/control
 
