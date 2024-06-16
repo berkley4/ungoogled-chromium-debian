berkley4/ungoogled-chromium-debian

commit 09e3ebbc067e72dc90509fdcb88b0292e7397f4a

shell launcher: Harden blocked flag processing
Blocked flags can be bypassed by specifying the
flag on the command line. This is arguably more
flexible as it allows the user to override the
block.

On the other hand, it's perhaps more useful from
a security perspective to not have this ability
to bypass.

Let's go with the latter by doing the blocked
flag processing directly after the positional
argument processing.

Anyone wanting to unblock a flag will now need
to have it removed from the blocked-flags file.

debian/misc_files/chromium.sh
@@ -102,17 +102,6 @@ for file in /etc/chromium.d/*; do
 done
 
 
-# Remove blocked flags
-if [ -n "$BLOCKED_FLAGS" ]; then
-  for flag in $BLOCKED_FLAGS; do
-    case $CHROMIUM_FLAGS in
-      *--$flag\ |*\ --$flag)
-        CHROMIUM_FLAGS="$(echo $CHROMIUM_FLAGS | sed -e "s@--$flag @@g" -e "s@ --$flag\$@@g")" ;;
-    esac
-  done
-fi
-
-
 # Inform the chrome binary that it has been run via a wrapper script
 export CHROME_WRAPPER=$0
 
@@ -162,6 +151,16 @@ while [ $# -gt 0 ]; do
       break ;;
   esac
 done
+
+# Remove blocked flags if any exist within CHROMIUM_FLAGS
+if [ -n "$BLOCKED_FLAGS" ]; then
+  for flag in $BLOCKED_FLAGS; do
+    case $CHROMIUM_FLAGS in
+      *--$flag\ |*\ --$flag)
+        CHROMIUM_FLAGS="$(echo $CHROMIUM_FLAGS | sed -e "s@--$flag @@g" -e "s@ --$flag\$@@g")" ;;
+    esac
+  done
+fi
 
 
 if [ $want_debug -eq 1 ] && [ ! -x $GDB ]; then

commit 7f4ee19e0680c6032c32ea215b5f8db870be4881

shell launcher: Fix command line runtime flag blocking
The problem was that flags specified on the command line
(except for --enable-remote-extensions) weren't getting
included in the CHROMIUM_FLAGS variable. This was part
of the original design rather than a regression.

Fix by making anything beginning with a double dash and
a lower case letter to be appended to CHROMIUM_FLAGS.

Fixes: 09e3ebbc067e72dc90509fdcb88b0292e7397f4a

debian/misc_files/chromium.sh
@@ -141,8 +141,8 @@ while [ $# -gt 0 ]; do
     --temp-profile )
       want_temp=1
       shift ;;
-    --enable-remote-extensions )
-      CHROMIUM_FLAGS="$CHROMIUM_FLAGS --enable-remote-extensions"
+    --[a-z]* )
+      CHROMIUM_FLAGS="$CHROMIUM_FLAGS $1"
       shift ;;
     -- ) # Stop option prcessing
       shift