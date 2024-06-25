berkley4/ungoogled-chromium-debian

commit 0baf60b484cd1fd65ddee182570d09bace7814bc

Suppress domsub warnings arising from DEPS.patch
Some paths used by the domain substitution script
contain no files as a result of the DEPS.patch
having been applied.

When domain substitution is done at the beginning
of a build, we get a ton of missing file warnings
because of this.

If we grep the DEPS file for one of the items
removed by DEPS.patch, then we can determine if
this patch has been applied or not.

Use the result of the grep command to set the
DEPS_PATCH variable in the configuration script.
If DEPS_PATCH=1 then the patch has been applied
and we can remove the following paths from the
UC submodule's pruning list :-

build/linux/debian_bullseye_i386-sysroot
build/linux/debian_bullseye_amd64-sysroot
third_party/blink/renderer/core/css/perftest_data
third_party/cros-components
third_party/crossbench
third_party/speedometer

debian/configure.sh
@@ -1000,17 +1000,31 @@ fi
 ##  Domain substitution, flags and pruning list ##
 ##################################################
 
+# Check whether DEPS.patch has been applied
+if [ $TEST -eq 0 ]; then
+  grep -q 'maps_perf_test' $RT_DIR/DEPS && DEPS_PATCH=0 || DEPS_PATCH=1
+fi
+
 ## Domain substitution
 DSB="$DSB -e \"/^chrome\/browser\/flag_descriptions\.cc/d\""
 DSB="$DSB -e \"/^content\/browser\/resources\/gpu\/info_view\.ts/d\""
 DSB="$DSB -e \"/^third_party\/depot_tools\//d\""
 DSB="$DSB -e \"/^tools\/clang\//d\""
 
+if [ $DEPS_PATCH -eq 1 ]; then
+  DSB="$DSB -e \"/^build\/linux\/debian_bullseye_i386-sysroot\//d\""
+  DSB="$DSB -e \"/^build\/linux\/debian_bullseye_amd64-sysroot\//d\""
+  DSB="$DSB -e \"/^third_party\/blink\/renderer\/core\/css\/perftest_data\//d\""
+  DSB="$DSB -e \"/^third_party\/cros-components\//d\""
+  DSB="$DSB -e \"/^third_party\/crossbench\//d\""
+  DSB="$DSB -e \"/^third_party\/speedometer\//d\""
+fi
 
-## Pruning/Submodule flags
+## Pruning
 PRU="$PRU -e \"/^chrome\/build\/pgo_profiles/d\""
 PRU="$PRU -e \"/^third_party\/depot_tools/d\""
 
+## Submodule flags
 if [ $PGO -eq 1 ]; then
   SMF="$SMF -e \"/^chrome_pgo_phase/d\""
 fi

commit d264216ec8b0bd65861433ebb2dceabe75a33d8b

configure.sh: Avoid DEPS_PATCH being undefined
This would have happened when TEST=1.

Fixes: 0baf60b484cd1fd65ddee182570d09bace7814bc

debian/configure.sh
@@ -998,9 +998,7 @@ fi
 ############################################################
 
 # Check whether DEPS.patch has been applied
-if [ $TEST -eq 0 ]; then
-  grep -q 'maps_perf_test' $RT_DIR/DEPS && DEPS_PATCH=0 || DEPS_PATCH=1
-fi
+[ $TEST -eq 0 ] && grep -q 'maps_perf_test' $RT_DIR/DEPS && DEPS_PATCH=0 || DEPS_PATCH=1
 
 ## Domain substitution
 DSB="$DSB -e \"/^chrome\/browser\/flag_descriptions\.cc/d\""