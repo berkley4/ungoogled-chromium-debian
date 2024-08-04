767d90ee5d2176f1570eb4738783969c069d227b
berkley4 <jeffblake0@gmail.com>
2024-08-04 17:59:47 +0100

    widevine-cdm.sh: Fix execution of script from widevine-cdm folder
    Firstly, rename the W_DIR variable to CUR_DIR. This is
    a bit more descriptive (ie it stands for current dir).
    
    The problem here is that CUR_DIR will evaluate to a blank
    string when executing from the directory containing the
    widevine-cdm.sh script.
    
    Fix this by forcing a null CUR_DIR value to '.' ie the
    shell notation for the current directory.

--- widevine-cdm/widevine-cdm.sh
+++ widevine-cdm/widevine-cdm.sh
@@ -1,15 +1,20 @@
 #!/bin/sh -e
 
-url=https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
-# ${example%/*} = $(dirname example)
-W_DIR=${0%/*}
-
-
 case $USER in
   root)
     printf '%s\n' "Run this script as an unprivileged user"
     exit 1 ;;
+esac
+
+
+url=https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# Directory containing this script (relative to where it's executed from).
+# Also force CUR_DIR to be '.' (current directory) if it evaluates to null.
+CUR_DIR=${0%/*}
+case $CUR_DIR in
+  "")
+    CUR_DIR='.' ;;
 esac
 
 
@@ -22,13 +27,13 @@ esac
 
 case $DL_CACHE in
   "")
-    DL_CACHE=$W_DIR/../download_cache ;;
+    DL_CACHE=$CUR_DIR/../download_cache ;;
 esac
 
 
 ## MAKE NEEDED DIRECTORIES
 
-for dir in $DL_CACHE $W_DIR/DEB/usr/lib; do
+for dir in $DL_CACHE $CUR_DIR/DEB/usr/lib; do
   [ -d $dir ] || mkdir -p $dir
 done
 
@@ -53,12 +58,12 @@ printf '\n%s\n' "Extracting files....."
 
 # ${url##*/} = $(basename $url)
 ar p $DL_CACHE/${url##*/} data.tar.xz | \
-  tar -C $W_DIR/DEB/usr/lib -xpJf - --strip-components 4 ./opt/google/chrome/WidevineCdm
+  tar -C $CUR_DIR/DEB/usr/lib -xpJf - --strip-components 4 ./opt/google/chrome/WidevineCdm
 
 
 ## GET/CHECK WIDEVINE VERSION
 
-w_man=$W_DIR/DEB/usr/lib/WidevineCdm/manifest.json
+w_man=$CUR_DIR/DEB/usr/lib/WidevineCdm/manifest.json
 w_ver=$(sed -n '/"version"/s@[":,a-z ]@@gp' $w_man)
 
 if [ $CHECK -eq 1 ]; then
@@ -72,12 +77,12 @@ fi
 
 ## BUILD DEB PACKAGE
 
-sed "s/@@W_VERSION@@/$w_ver/" < $W_DIR/control.in > $W_DIR/DEB/DEBIAN/control
+sed "s/@@W_VERSION@@/$w_ver/" < $CUR_DIR/control.in > $CUR_DIR/DEB/DEBIAN/control
 
-chmod 0755 $W_DIR/DEB/DEBIAN
+chmod 0755 $CUR_DIR/DEB/DEBIAN
 
-dpkg-deb --root-owner-group -z 9 -S extreme -b $W_DIR/DEB \
-  $W_DIR/widevine-cdm_${w_ver}_amd64.deb
+dpkg-deb --root-owner-group -z 9 -S extreme -b $CUR_DIR/DEB \
+  $CUR_DIR/widevine-cdm_${w_ver}_amd64.deb
 
 
 exit $?
