berkley4/ungoogled-chromium-debian

commit 4c7858bfd24fd0dac4cda956869ef7593953425c

configure.sh: Exempt google support urls from domain substitution
These urls are of the form https://support.google.com/* and
are accessible via various places in the user interface.

They are set in chrome/common/url_constants.h, so add this
file to the other exemptions in configure.sh.

This should be safe, as we are only permitting specific
urls which a user has a choice whether to click or not.

The blocking of anything which is potentially averse to
privacy (eg tracking), won't be affected by this change.

debian/configure.sh
@@ -1051,6 +1051,7 @@ fi
 
 ## Domain substitution
 DSB="$DSB -e \"/^chrome\/browser\/flag_descriptions\.cc/d\""
+DSB="$DSB -e \"/^chrome/common/url_constants\.h/d\""
 DSB="$DSB -e \"/^content\/browser\/resources\/gpu\/info_view\.ts/d\""
 DSB="$DSB -e \"/^tools\/clang\//d\""
 

commit 6858d73cc6f74034abd0055ab1a4d2e6af552a55

configure.sh: Fix exempting google support urls from domsub
This is a copy-paste error. Fix by escaping the slashes
in the file path.

Fixes: 4c7858bfd24fd0dac4cda956869ef7593953425c

debian/configure.sh
@@ -1051,7 +1051,7 @@ fi
 
 ## Domain substitution
 DSB="$DSB -e \"/^chrome\/browser\/flag_descriptions\.cc/d\""
-DSB="$DSB -e \"/^chrome/common/url_constants\.h/d\""
+DSB="$DSB -e \"/^chrome\/common\/url_constants\.h/d\""
 DSB="$DSB -e \"/^content\/browser\/resources\/gpu\/info_view\.ts/d\""
 DSB="$DSB -e \"/^tools\/clang\//d\""
 