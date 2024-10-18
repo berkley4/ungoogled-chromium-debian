berkley4/ungoogled-chromium-debian

commit 9d8ad300ec708c5778a3ef6d1692a86ddc75ec09

Fix optional/fixes/icudata-file-path.patch
We need to use v8_depend_on_icu_data_file in the
patch context instead of icu_use_data_file, with
the former being determined by the value of the
latter.

debian/configure.sh
@@ -1187,6 +1187,7 @@ else
   op_disable="$op_disable fixes/icudata-file-path fixes/skia-allow-bundled-harfbuzz"
 
   gn_disable="$gn_disable icu_copy_icudata_to_root_build_dir=false"
+  gn_disable="$gn_disable v8_depend_on_icu_data_file"
 
   # GN_FLAGS += icu_use_data_file=false use_system_harfbuzz=true
   gn_enable="$gn_enable icu_use_data_file=false"

debian/patches/optional/fixes/icudata-file-path.patch
@@ -1,9 +1,9 @@
 --- a/v8/BUILD.gn
 +++ b/v8/BUILD.gn
-@@ -2454,11 +2454,10 @@
+@@ -2440,11 +2440,10 @@
      sources = []
  
-     if (icu_use_data_file) {
+     if (v8_depend_on_icu_data_file) {
 -      deps += [ "//third_party/icu:copy_icudata" ]
        if (host_byteorder == "big") {
 -        sources += [ "$root_out_dir/icudtb.dat" ]

commit f9fe1cebcea137251a9d6f657e4ca61cfd3f323d

configure.sh: Remove reference to v8_depend_on_icu_data_file
This wrongly got left in. As stated in the original
commit message, v8_depend_on_icu_data_file is set by
the value of icu_use_data_file.

Fixes: 9d8ad300ec708c5778a3ef6d1692a86ddc75ec09

debian/configure.sh
@@ -1185,9 +1185,7 @@ if [ $SYS_ICU -eq 0 ]; then
   RUL="$RUL -e \"/icudtl.dat/s@#@@\""
 else
   op_disable="$op_disable fixes/icudata-file-path fixes/skia-allow-bundled-harfbuzz"
-
   gn_disable="$gn_disable icu_copy_icudata_to_root_build_dir=false"
-  gn_disable="$gn_disable v8_depend_on_icu_data_file"
 
   # GN_FLAGS += icu_use_data_file=false use_system_harfbuzz=true
   gn_enable="$gn_enable icu_use_data_file=false"