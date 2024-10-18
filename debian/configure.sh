berkley4/ungoogled-chromium-debian

commit ec25c6263277d5a20c0355c3e97a9ebe403ea450

configure.sh: Fix enabling system tiff when PDF_JS=1
The use_system_libtiff=true flag somehow went missing
from debian/rules, but is needed by pdf js/xfa forms.

The logic was also wrong in configure.sh, as the flag
needs to be enabled (not disabled) and should therefore
be acted on separately to the pdf flags.

This affects PDF_JS=1 only, which is not the default.

debian/configure.sh
@@ -870,9 +870,9 @@ fi
 
 
 if [ $PDF_JS -eq 1 ]; then
-  # GN_FLAGS += pdf_enable_v8=false pdf_enable_xfa=false
-  # GN_FLAGS += use_system_libtiff=true
+  gn_enable="$gn_enable use_system_libtiff"
   gn_disable="$gn_disable pdf_enable_v8 use_system_libtiff"
+  deps_enable="$deps_enable libtiff"
 
   # Prevent libzstd being enabled twice
   if [ $ZSTD -eq 0 ]; then

debian/control.in
@@ -40,7 +40,6 @@ Build-Depends:
  libevent-dev,
  liblcms2-dev,
  libopenjp2-7-dev,
- libtiff-dev,
  libdouble-conversion-dev,
  libxnvctrl-dev,
  libopenh264-dev,
@@ -55,6 +54,7 @@ Build-Depends:
 #libharfbuzz-dev,
 #libicu-dev (>= 73),
 #libpng-dev,
+#libtiff-dev,
 #libxslt1-dev,
 #libzstd-dev
 

debian/rules.in
@@ -167,8 +167,11 @@ GN_FLAGS += enable_mutex_priority_inheritance=true
 # Disable openh264
 #GN_FLAGS += media_use_openh264=false rtc_use_h264=false
 
-# Disable pdfium javascript and XFA forms support
+# Disable pdfium javascript/XFA forms support
 GN_FLAGS += pdf_enable_v8=false pdf_enable_xfa=false
+
+# Enable when enabling pdfium javascript/XFA forms support
+#GN_FLAGS += use_system_libtiff=true
 
 # Enable pipewire
 GN_FLAGS += rtc_use_pipewire=true

commit 7b5429449775f017b13bad4d3832d24eb60db2bd

debian/configure.sh: Fix pdf/system tiff stuff
I missed out removing the 'use_system_libtiff' bit
from the gn_disable.

Fixes: ec25c6263277d5a20c0355c3e97a9ebe403ea450

debian/configure.sh
@@ -868,7 +868,7 @@ fi
 
 if [ $PDF_JS -eq 1 ]; then
   gn_enable="$gn_enable use_system_libtiff"
-  gn_disable="$gn_disable pdf_enable_v8 use_system_libtiff"
+  gn_disable="$gn_disable pdf_enable_v8"
   deps_enable="$deps_enable libtiff"
 
   # Prevent libzstd being enabled twice