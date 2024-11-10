berkley4/ungoogled-chromium-debian

commit d696b838bb10d50e5b8a4d6d233796bfb7a2e352

configure.sh: Do not install google-lens flag file when LENS=0
There's no point in installing a flag file for something that
has been disabled at build time. Since LENS is now disabled
by default, we can stop installing its flag file [1].

[1] /etc/chromium.d/google-lens

debian/configure.sh
@@ -837,6 +837,7 @@ if [ $LENS -eq 0 ]; then
   ins_disable="$ins_disable google-lens"
 else
   DSB="$DSB -e \"/^components\/lens\/lens_features\.cc/d\""
+  INS="$INS -e \"/google-lens/s@^#@@\""
 
   if [ $LENS -ge 2 ]; then
     GOOGLE_API_KEYS=2

debian/ungoogled-chromium.install.in
@@ -41,7 +41,7 @@ debian/etc/chromium.d/anti-audio-fingerprint                                   e
 debian/etc/chromium.d/anti-phish-track                                         etc/chromium.d
 debian/etc/chromium.d/blocked-flags                                            etc/chromium.d
 debian/etc/chromium.d/google-api-keys                                          etc/chromium.d
-debian/etc/chromium.d/google-lens                                              etc/chromium.d
+#debian/etc/chromium.d/google-lens                                              etc/chromium.d
 debian/etc/chromium.d/google-translate                                         etc/chromium.d
 debian/etc/chromium.d/gpu                                                      etc/chromium.d
 debian/etc/chromium.d/hls-player                                               etc/chromium.d

commit 2abcabc01d9a378a7869c66642b72e64f2508ea9

configure.sh: Use ins_{en,dis}able instead of INS
I forgot that we have a simpler way to enable/disable
file installation, and what follows illustrates its
utility,

I screwed up the logic on disabling the installation of
google-api-keys. The INS expression was trying to enable
instead of disable installation, so use an ins_disable
in its place.

Also, the ins_disable under the LENS=0 conditional is
superfluous since LENS=0 is now the default.

Fixes: d696b838bb10d50e5b8a4d6d233796bfb7a2e352

debian/configure.sh
@@ -834,10 +834,9 @@ fi
 
 if [ $LENS -eq 0 ]; then
   gn_enable="$gn_enable enable_lens_desktop=false"
-  ins_disable="$ins_disable google-lens"
 else
+  ins_enable="$ins_enable google-lens"
   DSB="$DSB -e \"/^components\/lens\/lens_features\.cc/d\""
-  INS="$INS -e \"/google-lens/s@^#@@\""
 
   if [ $LENS -ge 2 ]; then
     GOOGLE_API_KEYS=2
@@ -1066,7 +1065,7 @@ fi
 
 ## Enable Google API keys for google services
 if [ $GOOGLE_API_KEYS -eq 0 ]; then
-  INS="$INS -e \"/google-api-keys/s@^#@@\""
+  ins_disable="$ins_disable google-api-keys"
 elif [ $GOOGLE_API_KEYS -ge 2 ]; then
   sed -e '/^#export GOOGLE_/s@^#@@' -i $FLAG_DIR/google-api-keys
 fi