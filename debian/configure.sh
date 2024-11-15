9a203b9b90eea153e0a8fd0cca6c1b0c61339d98
berkley4 <jeffblake0@gmail.com>
2024-11-15 14:14:02 +0000

    Fix ffmpeg hevc (FF_HEVC=1) build
    There are several changes here. We need change source
    filenames and paths due to upstream ffmpeg changes.
    
    We need to add libavcodec/container_fifo.c to the build
    sources since it contains functions needed by hevc.
    
    We add third_party/ffmpeg/libavcodec to a couple of
    include_dirs directives so that the build system can
    find various header files.
    
    Some of the hevc source filenames clash with other
    ffmpeg source files, resulting in build errors.
    To  avoid this I have added renamed some files in
    third_party/ffmpeg/ffmpeg_generated.gni and added
    some code to d/rules to add symlinks (with the new
    filename) to the orignal files.

--- debian/configure.sh
+++ debian/configure.sh
@@ -793,6 +793,9 @@ fi
 if [ $FF_HEVC -eq 0 ]; then
   op_disable="$op_disable ffmpeg-extra-codecs/hevc/"
   gn_enable="$gn_enable enable_platform_hevc=false"
+
+  RUL="$RUL -e \"/^HEVC_/s@^@#@\""
+  RUL="$RUL -e \"/libavcodec-hevc-/s@\(for\)@#\1@\""
 fi
 
 
