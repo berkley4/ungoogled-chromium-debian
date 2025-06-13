0fce563e02b951d94d7176d5361f9aebe399eca0
berkley4 <jeffblake0@gmail.com>
2025-06-13 14:14:06 +0100

    build-bindgen.sh: Bump bindgen version to 0.72.0

--- build/build-bindgen.sh
+++ build/build-bindgen.sh
@@ -21,7 +21,7 @@ esac
 
 DL_CACHE=.download_cache
 
-bg_tag=upstream/v0.71.1
+bg_tag=upstream/v0.72.0
 nc_ver=Ws0ru48A4IYoYLVKbV5K5_mDYT4ml9LAQUKdkiczdlMC
 
 bg_repo=https://chromium.googlesource.com/external/github.com/rust-lang/rust-bindgen.git
