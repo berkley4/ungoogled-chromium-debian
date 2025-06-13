633c3574c7b81cd0fbd311016893c5631878027c
berkley4 <jeffblake0@gmail.com>
2025-06-13 13:45:18 +0100

    configure.sh: Enable rusty-png by default
    It seems stable enough, so have this do our
    png decoding by default.
    
    Those that prefer libpng can comment out the
    flag in /etc/chromium.d/miscellaneous.

--- debian/configure.sh
+++ debian/configure.sh
@@ -107,7 +107,7 @@ POLICIES=etc/chromium/policies/managed/policies.json
 [ -n "$PRINT_PREVIEW" ] || PRINT_PREVIEW=1
 [ -n "$PULSE" ] || PULSE=1
 [ -n "$QT" ] || QT=1
-[ -n "$RUSTY_PNG" ] || RUSTY_PNG=0
+[ -n "$RUSTY_PNG" ] || RUSTY_PNG=1
 [ -n "$SKIA_GAMMA" ] || SKIA_GAMMA=0
 [ -n "$SPEECH" ] || SPEECH=1
 [ -n "$SPOOF_WEBGL_INFO" ] || SPOOF_WEBGL_INFO=1
@@ -1235,8 +1235,8 @@ if [ $PULSE -eq 0 ]; then
 fi
 
 
-if [ $RUSTY_PNG -eq 1 ]; then
-  sed -e '/rusty-png/s@^#@@' -i $FLAG_DIR/miscellaneous
+if [ $RUSTY_PNG -eq 0 ]; then
+  sed -e '/rusty-png/s@^@#@' -i $FLAG_DIR/miscellaneous
 fi
 
 
