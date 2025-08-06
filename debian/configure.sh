38bdb605b60bce46114c0a21f54e6b982b698253
berkley4 <jeffblake0@gmail.com>
2025-08-06 08:46:10 +0100

    configure.sh: Fix handling of audio codecs patches
    I somehow forgot to remove the part that was
    supposed to be replaced.
    
    Fixes 8036c3dec4280a8c9095914d8def954e51873630

--- debian/configure.sh
+++ debian/configure.sh
@@ -1228,14 +1228,6 @@ case $FF_AC in
         -i $OP_DIR/ffmpeg-extra-codecs/audio-codecs.patch ;;
 esac
 
-if [ $FF_AAC -eq 1 ] && [ $FF_AC3 -eq 0 ] && [ $FF_AC4 -eq 0 ] && \
-   [ $FF_ALAC -eq 0 ] && [ $FF_FDK -eq 0]; then
-  op_disable="$op_disable ffmpeg-extra-codecs/audio-codecs.patch"
-  op_disable="$op_disable ffmpeg-extra-codecs/context-fixup.patch"
-else
-  sed -e "s@_ff_ac@$FF_AC@" -i $OP_DIR/ffmpeg-extra-codecs/audio-codecs.patch
-fi
-
 
 
 ## Enable Google API keys for google services
