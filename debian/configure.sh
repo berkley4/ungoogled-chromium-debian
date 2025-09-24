6822410678dd4ddee5687bf2176f431a2d270a33
berkley4 <jeffblake0@gmail.com>
2025-09-24 08:33:30 +0100

    configure.sh: Allow re-enabling RSA 16k key support
    Upstream (boringssl) recently removed support for
    RSA 16k keys.
    
    Allow users to access websites which are configured
    to use such keys via enabling a revert patch from
    the freebsd project.
    
    The new RSA_16K configuration variable controls
    enablement (disabled by default).

--- debian/configure.sh
+++ debian/configure.sh
@@ -107,6 +107,7 @@ UC_P_DIRS="$UC_DIR/patches/core $UC_DIR/patches/extra"
 [ -n "$PULSE" ] || PULSE=1
 [ -n "$QT" ] || QT=1
 [ -n "$QT_6" ] || QT_6=0
+[ -n "$RSA_16K" ] || RSA_16K=0
 [ -n "$RUSTY_PNG" ] || RUSTY_PNG=1
 [ -n "$SKIA_GAMMA" ] || SKIA_GAMMA=0
 [ -n "$SPEECH" ] || SPEECH=1
@@ -1140,6 +1141,11 @@ fi
 if [ $PRINT_PREVIEW -eq 0 ]; then
   # GN_FLAGS += enable_print_preview=false enable_oop_printing=false
   gn_enable="$gn_enable enable_print_preview=false"
+fi
+
+
+if [ $RSA_16K -eq 1 ]; then
+  op_enable="$op_enable return-RSA-16384-key-support.patch"
 fi
 
 
