64c7f7586fd252bb0f2bd0539f97025b00af1336
berkley4 <jeffblake0@gmail.com>
2026-07-15 02:24:49 +0100

    Drop optional/translate/revert-disable.patch
    Now that upstream UC isn't crippling google translate
    functionality, we can drop this.
    
    The remainder of configuration via setting the TRANSLATE
    variable remains unchanged, with the unsupported switch
    warning patch moved and renamed.

--- debian/configure.sh
+++ debian/configure.sh
@@ -1093,7 +1093,7 @@ fi
 
 
 if [ $TRANSLATE -eq 0 ]; then
-  op_disable="$op_disable translate/"
+  op_disable="$op_disable translate-stop-unsupported-switch-warning.patch"
   ins_disable="$ins_disable google-translate"
 else
   DSB="$DSB -e \"/\/translate_manager_browsertest\.cc/d\""
